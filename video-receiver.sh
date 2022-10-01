aa3fa7cba78f27370993a816ac929bf010f509a3
Stanislaw Pusep <creaktive@gmail.com>
2022-10-01 11:47:17 +0200

    bigger video

--- video-receiver.sh
+++ video-receiver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # https://tellopilots.com/threads/tello-video-web-streaming.455/#post-13986
-exec ffplay -probesize 32 -i udp://@:11111 -framerate 30
+exec ffplay -probesize 32 -i udp://@:11111 -framerate 30 -vf scale=1440:-1
